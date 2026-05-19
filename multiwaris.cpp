1b6b80dc81224dc4f2ef464619521e7b35e60f4d
suhilmi01 <suhilmi01@mail.com>
2026-05-19 17:14:51 +0700

    fungsi utama

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
 
-class orang
-{
+class orang {
 public:
     int umur;
+    orang(int pUmur) : 
+        umur(pUmur) 
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
 
-    orang(int pUmur) : umur(pUmur)
-    {
-        cout << "orang dibuat dengan umur " << umur << "\n"
-             << endl;
-    }
-};
-class pelajar : virtual public orang
-{
+class pekerja : virtual public orang {
 public:
-    pelajar(int pUmur) : orang(pUmur)
+    pekerja(int pUmur) : 
+        orang(pUmur) 
     {
-        cout << "Pelajar dibuat\n"
-             << endl;
+        cout << "pekerja dibuat\n" << endl;
     }
 };
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : 
+        orang(pUmur) 
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) : 
+        pekerja(pUmur), 
+        pelajar(pUmur), 
+        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
